25caed4cf4765cb65fc286f0c222ff7842428bfa
Barri <info@bdesignstudio.co.uk>
2020-11-17 10:48:51 +0000

    encode the enter string

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.querySelector('form').addEventListener('submit' , (e)=>{
   e.preventDefault();
-  const input= document.querySelector('input')
-  console.log(input.value)
-
+  const input= document.querySelector('#message-input')
+  //console.log(input.value)
+const encrypted = btoa(input.value);
+ document.querySelector('#link-input').value = encrypted;
 })
